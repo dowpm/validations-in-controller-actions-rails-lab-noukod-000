d2367edd3df4d6209475e24d75bcddf3a796d354
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-07 20:31:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,12 +10,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    if @post
-      redirect_to post_path(@post)
-    else
-      render :new
-    end
-
+    redirect_to post_path(@post)
   end
 
   private
